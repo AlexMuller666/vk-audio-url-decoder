3931b4ce9d74e581001e2215e01304da800d2d53
1271 <sttv-pc@mail.ru>
2018-03-02 15:38:56 +0300

    change setup.py, change docs

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # VK audio url decoder [![Build Status](https://travis-ci.org/yuru-yuri/vk-audio-url-decoder.svg?branch=master)](https://travis-ci.org/yuru-yuri/vk-audio-url-decoder)
 
+[![PyPI - License](https://img.shields.io/pypi/l/vaud.svg)](https://pypi.org/project/vaud/)
+[![GitHub issues](https://img.shields.io/github/issues/yuru-yuri/vk-audio-url-decoder-php.svg)](https://github.com/yuru-yuri/vk-audio-url-decoder-php/issues)
 [![PyPI](https://img.shields.io/pypi/v/vaud.svg)](https://pypi.org/project/vaud/)
+
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/f88a8b485070badb584b/maintainability)](https://codeclimate.com/github/yuru-yuri/vk-audio-url-decoder/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/f88a8b485070badb584b/test_coverage)](https://codeclimate.com/github/yuru-yuri/vk-audio-url-decoder/test_coverage)
 
 ## Usage:
 
--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 
-import os
-
 from setuptools import setup
 from vaud import __version__
-
-here = os.path.abspath(os.path.dirname(__file__))
 
 requires = []
 
@@ -16,13 +12,11 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "License :: OSI Approved :: MIT License",
@@ -31,8 +25,8 @@ setup(
     author="Zharkov Sergey",
     author_email="sttvpc@gmail.com",
     url="https://github.com/yuru-yuri/vk-audio-url-decoder/",
-    download_url = 'https://github.com/yuru-yuri/vk-audio-url-decoder/archive/' + __version__ + '.tar.gz',
-    license="MIT License (https://opensource.org/licenses/MIT)",
+    download_url='https://github.com/yuru-yuri/vk-audio-url-decoder/archive/' + __version__ + '.tar.gz',
+    license="MIT",
     packages=['vaud'],
     include_package_data=False,
     zip_safe=False,
